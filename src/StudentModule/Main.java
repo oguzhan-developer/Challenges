b8f66db36c44fd7bae38348b58f276813225b84f
Oguzhan YALCIN <oguzhanfinal@gmail.com>
2022-12-05 21:05:29 +0300

    .

--- src/StudentModule/Main.java
+++ src/StudentModule/Main.java
@@ -1,7 +1,7 @@
 package StudentModule;
 
 import java.util.TreeSet;
-
+import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
         TreeSet<Student> students = new TreeSet<>(new StudentComparator().reversed());
@@ -10,10 +10,17 @@ public class Main {
         students.add(new Student("Halil",10));
         students.add(new Student("Pelin",40));
 
+        /*
         for(Student s: students)
         {
             System.out.println(s.getName()+" " + s.getNote());
         }
-
+        */
+        Iterator<Student> itr = students.iterator();
+        while(itr.hasNext())
+        {
+            Student tempS = itr.next();
+            System.out.println(tempS.getName()+" "+tempS.getNote());
+        }
     }
 }
